5d1d125ce828a11ddd3525ff9d27196689812ea7
jicasares94@gmail.com <Cachorrillo3,14>
2020-11-27 23:21:10 +0100

    oh oh made an oopsie!

--- src/server/auth.js
+++ src/server/auth.js
@@ -21,22 +21,21 @@ passport.deserializeUser((id, done) => {
         .catch((err) => done(err, null));
 });
 
-passport.use(new LocalStrategy(options, (username, password, done) => {
-    User.findOne({ username })
+passport.use(new LocalStrategy((username, password, done) => {
+    User.findOne({username})
         .then((user) => {
             if (!user) {
                 return done(null, false);
             }
 
-            bcrypt.compare(password, user.password)
-                .then(() => {
-                    return done(null, user);
-                })
-                .catch(() => {
-                    return done(null, false);
-                });
+            const validate = bcrypt.compareSync(password, user.password);
+            if (validate) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
         })
         .catch((err) => done(err, null));
-}));
+}, options));
 
 export default passport;
